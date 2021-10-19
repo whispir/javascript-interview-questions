d5199b1beb53767f4bbb1a21f8e3d515699a63ce
David Johnston <djohnston@whispir.com>
2021-10-20 07:47:37 +1100

    Add new exercises

---
+++ src/exercise2.js
@@ -0,0 +1,41 @@
+
+//ex1.
+
+// For a given list of items A, and list of items B: 
+// For all of the items B that have color: red - find the corresponding value in list A
+// And sum them
+
+//eg.
+// const listA = [
+//     {
+//         itemType: "apple",
+//         value: 2
+//     },
+//     {
+//         itemType: "cherry",
+//         value: 10
+//     },
+//     {
+//         itemType: "melon",
+//         value: 3
+//     }
+// ];
+
+// const listB = [
+//     {
+//         itemType: "apple",
+//         color: "red",
+//     },
+//     {
+//         itemType: "apple",
+//         color: "green",
+//     },
+//     {
+//         itemType: "melon",
+//         color: "red",
+//     },
+// ];
+
+//Expected total: 5
+
+
---
+++ src/exercise3.js
@@ -0,0 +1,3 @@
+
+// Like the previous example, but lets say that List A always remains the same, but we are going to call the function with three different list Bs. 
+// How would you implement this?
